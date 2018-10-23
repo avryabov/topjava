41a02747e93d799cd2f412c082d77bdaf0721377
avryabov <antonrbv@gmail.com>
2018-10-23 22:30:05 +0300

    hw2 added scripts for meals db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,22 @@
-DELETE FROM user_roles;
-DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE
+FROM meals;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+ALTER SEQUENCE global_seq
+  RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calorioes)
+VALUES (100000, '2015-05-1 14:00:00', 'User dinner', 510),
+       (100000, '2015-05-1 21:00:00', 'User supper', 1500),
+       (100001, '2015-05-1 14:00:00', 'Admin dinner', 510),
+       (100001, '2015-05-1 20:20:20', 'Admin supper', 500);
